7057f4c05346c5919b4cf99ec2113d731259d61e
Masaru Takahashi <matakaha@microsoft.com>
2022-04-28 15:46:06 +0900

    service1作成(c2dメッセージ送信)

---
+++ service1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Devices;
+using System.Text;
+namespace service1
+{
+
+    class Program
+    {
+        static string connectionString = "{Your IoT Hub connection string here}";
+        static string targetDevice = "{deviceId}";
+        private static async Task Main(string[] args)
+        {
+            ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
+            await serviceClient.SendAsync(targetDevice, commandMessage);
+        }
+    }
+}
